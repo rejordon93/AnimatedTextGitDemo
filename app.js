edeb109379d6f53dca7b9ee813e9872153557be5
Ethan Jordon <rejordon17@gmail.com>
2022-11-28 01:05:33 -0800

    add v1 of color script

---
+++ app.js
@@ -0,0 +1,13 @@
+function randomRGB() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb()${r},${g},${b}`;
+}
+
+const letters = document.querySelectorAll(".latter");
+const intervalid = setInterval(function () {
+  for (const letter of letters) {
+    letter.style.color = randomRGB();
+  }
+}, 2000);

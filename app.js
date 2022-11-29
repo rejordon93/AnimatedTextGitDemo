bb7cf958bca57ca74e3a06f58a59b10f89c99b92
Ethan Jordon <rejordon17@gmail.com>
2022-11-29 00:19:57 -0800

    commit

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ function randomRGB() {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
-  return `rgb()${r},${g},${b}`;
+  return `rgb(${r},${g},${b})`;
 }
 
-const letters = document.querySelectorAll(".latter");
+const letters = document.querySelectorAll(".letter");
 const intervalid = setInterval(function () {
   for (const letter of letters) {
     letter.style.color = randomRGB();
+    letter.style.fontSize = "3rem";
   }
-}, 2000);
+}, 1000);
